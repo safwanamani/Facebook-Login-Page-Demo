138eed98a300c891ef395b5a0f915174a9cc0d08
Safwan Amani <safwanamanikps@gmail.com>
2020-07-13 21:14:20 +0530

    first commit

---
+++ js/fb.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $("#signup-form").validate({
+        rules:{
+            fname:{
+                required:true,
+                minlength:4,
+                maxlength:8
+            },
+            sname:{
+                required:true,
+                minlength:4
+            },
+            emailAddress:{
+                required:true,
+                email:true
+
+            },
+            password:{
+                minlength:6
+            },
+            day:{
+                required:true
+            },
+            gender:{
+                required:true
+            }
+        },
+        messages:{
+            fname:{
+                required:"First name is required",
+            }
+        }
+    })
+})
